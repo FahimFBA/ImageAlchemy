1528231c667844e2f70ad8bb36bb902f9d71c523
FahimFBA <fahimbinamin@gmail.com>
2024-03-02 00:52:10 +0600

    update: project name

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const IBMPlex = IBM_Plex_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "ImagedAI",
+  title: "ImageAlchemy",
   description: "An all-in-one tool for the Digital Creators",
 };
 
