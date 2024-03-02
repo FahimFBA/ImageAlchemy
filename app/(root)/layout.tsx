62fe6de914eebf66779cbc7ccb271fb651bead55
FahimFBA <fahimbinamin@gmail.com>
2024-03-02 13:36:29 +0600

    fix: upper user button

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,17 +1,20 @@
+import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
-import React from 'react'
+
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <Sidebar />
-      {/* <MobileNav /> */}
+      <MobileNav />
 
-        <div className="root-container">
-            <div className="wrapper">
-                {children}
-            </div>
+      <div className="root-container">
+        <div className="wrapper">
+          {children}
         </div>
+      </div>
+
+
     </main>
   )
 }
