a8bac2ca263931a8fe676bd07b14a249caae4323
FahimFBA <fahimbinamin@gmail.com>
2024-03-02 12:54:09 +0600

    fix: navbar button

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -4,43 +4,49 @@ import { SignedIn } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-// import React from 'react'
+
 
 const Sidebar = () => {
   const pathname = usePathname();
   return (
     <aside className="sidebar">
-        <div className="flex size-full flex-col gap-4">
-            <Link href="/" className="sidebar-logo">
-                <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
-            </Link>
+      <div className="flex size-full flex-col gap-4">
+        <Link href="/" className="sidebar-logo">
+          <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
+        </Link>
 
-            <nav className="sidebar-nav">
-              <SignedIn>
-                <ul className="sidebar-nav_elements">
-                  {navLinks.map((link) => {
-                    const isActive = link.route === pathname;
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.slice(0, 6).map((link) => {
+                const isActive = link.route === pathname
 
-                    return (
-                      <li key={link.route} className={`sidebar-nav_element group $ {
-                        isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
-                      }`}>
-                        {link.label}
+                return (
+                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                    }`}>
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && 'brightness-200'}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
 
-                      </li>
-                    )
+          </SignedIn>
 
-                  })}
-                </ul>
-
-              </SignedIn>
-
-            </nav>
+        </nav>
 
 
 
 
-        </div>
+      </div>
 
     </aside>
   )
