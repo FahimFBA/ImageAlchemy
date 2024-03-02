8b2c72ea437003c3cd012dd7f2f2d216245bce38
FahimFBA <fahimbinamin@gmail.com>
2024-03-02 13:05:20 +0600

    finish: ui-sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
-import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
   weight: ['400', '500', '600', '700'],
   variable: '--font-ibm-plex'
-
 });
+
 
 export const metadata: Metadata = {
   title: "ImageAlchemy",
